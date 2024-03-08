e546ab0aea4d8cf0a3c092ac05299502d25ff05c
Likhith-dev007 <liki00333@gmail.com>
2024-03-07 19:24:34 -0500

    second

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Studying
- javascript
+ javascript is awesome
+ 
---
+++ script.js
@@ -0,0 +1,13 @@
+let a = 1;
+const friends = [1, 2, 3]
+console.log(friends[2]);
+console.log(a);
+console.log(friends.length);
+
+function abc(){
+  console.log(20)
+  console.log(500)
+
+}
+
+abc()
