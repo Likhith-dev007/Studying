356f93a0387601321ac0152804553aeeeb603577
Likhith-dev007 <liki00333@gmail.com>
2024-03-10 22:22:47 -0400

    third

--- script.js
+++ script.js
@@ -1,13 +1,95 @@
-let a = 1;
-const friends = [1, 2, 3]
-console.log(friends[2]);
-console.log(a);
-console.log(friends.length);
+// const button1 = document.getElementById('btn1');
+// const button2 = document.getElementById('btn2');
+// const h1 = document.getElementById('abc')
+// let count = 0;
 
-function abc(){
-  console.log(20)
-  console.log(500)
+// button1.addEventListener('click', function() {
+//   count++;
+//   console.log(count);
+
+//   //create an element
+
+//   const li = document.createElement('li')
+//   li.innerHTML = '<b>something</b>' + count
+//   li.style.backgroundColor = "red"
+
+
+//   //append tht element
+
+//   h1.appendChild(li)
+  
+// });
+
+// button2.addEventListener('click', function() {
+//   count--;
+//   console.log(count);
+
+//   const li = document.createElement('li')
+//   li.innerHTML = '<b>something</b>' + count
+
+
+//   //append tht element
+
+//   h1.appendChild(li)
+// });
+
+
+
+// const li = document.createElement('li')
+// // const text = document.createTextNode('sentence' + count)
+// li.textContent = 'dynaimc'+ count;
+// // li.appendChild(text)
+// h1.appendChild(h1)
+
+
+
+
+//ES6
+
+
+   const isafunction = (b) => {
+    b = 200* b;
+    console.log(b)
+
+   }
+
+
+isafunction(1000)
+ 
+
+const arrayy = [1,2,3,4,5,6]
+for(let i=0; i<arrayy.length; i++){
+  const el = arrayy[i]
 
 }
 
-abc()
+const newmap = arrayy.map(function(element){
+  console.log(element)
+  return 100
+
+})
+
+console.log(arrayy, newmap)
+
+
+
+let consray = `djnjended
+
+
+
+cejrfrf
+frjnjmsncjdnj`
+console.log(consray)
+
+
+
+const promobj = fetch('/data.json')
+promobj.then(Response => {
+  const promobj2 = Response.json()
+  promobj2.then(data => {
+    console.log(data)
+  })
+  return null
+}).then(finaldata => {
+  console.log('final dats is ', finaldata)
+})
---
+++ script1.js
@@ -0,0 +1,43 @@
+// // let a = 1;
+// // const friends = [1, 2, 3]
+// // console.log(friends[2]);
+// // console.log(a);
+// // console.log(friends.length);
+
+// // function abc(a, b) {
+// //   for (let i = 1; i <= a; i++) {
+// //     if (i % 2 == 0) {
+// //       console.log(i)
+// //     }
+// //   }
+// // }
+
+// // abc(20, 999);
+
+// // // let addition = []
+// // // function abc(name){
+// // //   addition.push(name);
+// // //   console.log(addition);
+
+// // // }
+
+// // // abc('loud')
+// // // abc('louda')
+// // // abc('loude')
+
+// // // console.log (addition.length)
+
+
+// const myobj = {
+//   key1: 'escn',
+//   key2: 'rcb'
+// }
+
+// console.log(myobj.key2)
+
+const heading = document.querySelector('h1')
+heading.innerText = "loud"
+console.log(heading)
+
+
+
